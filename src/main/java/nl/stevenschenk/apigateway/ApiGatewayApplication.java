2cc58a26e9a3ebfcbca6e9cbf524ef3cb00c4042
Steven Schenk <stevenschenk74@hotmail.com>
2019-05-12 14:17:56 +0200

    Initial commit

---
+++ src/main/java/nl/stevenschenk/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,25 @@
+package nl.stevenschenk.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/uren")
+                        .uri("http://google.nl"))
+                .build();
+    }
+
+}
